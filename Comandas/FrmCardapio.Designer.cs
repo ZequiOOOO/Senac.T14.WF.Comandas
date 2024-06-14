689925b50ec7695633dfb63067597d94c41e7acc
Ezequi0000 <ezequielramos2299@gmail.com>
2024-06-13 21:39:36 -0300

    final de aula hoje

--- Comandas/FrmCardapio.Designer.cs
+++ Comandas/FrmCardapio.Designer.cs
@@ -28,55 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            cyberButton1 = new ReaLTaiizor.Controls.CyberButton();
+            btnvoltar = new ReaLTaiizor.Controls.CyberButton();
             SuspendLayout();
             // 
-            // cyberButton1
+            // btnvoltar
             // 
-            cyberButton1.Alpha = 20;
-            cyberButton1.BackColor = Color.Transparent;
-            cyberButton1.Background = true;
-            cyberButton1.Background_WidthPen = 4F;
-            cyberButton1.BackgroundPen = true;
-            cyberButton1.ColorBackground = Color.FromArgb(37, 52, 68);
-            cyberButton1.ColorBackground_1 = Color.FromArgb(37, 52, 68);
-            cyberButton1.ColorBackground_2 = Color.FromArgb(41, 63, 86);
-            cyberButton1.ColorBackground_Pen = Color.FromArgb(29, 200, 238);
-            cyberButton1.ColorLighting = Color.FromArgb(29, 200, 238);
-            cyberButton1.ColorPen_1 = Color.FromArgb(37, 52, 68);
-            cyberButton1.ColorPen_2 = Color.FromArgb(41, 63, 86);
-            cyberButton1.CyberButtonStyle = ReaLTaiizor.Enum.Cyber.StateStyle.Custom;
-            cyberButton1.Effect_1 = true;
-            cyberButton1.Effect_1_ColorBackground = Color.FromArgb(29, 200, 238);
-            cyberButton1.Effect_1_Transparency = 25;
-            cyberButton1.Effect_2 = true;
-            cyberButton1.Effect_2_ColorBackground = Color.White;
-            cyberButton1.Effect_2_Transparency = 20;
-            cyberButton1.Font = new Font("Arial", 11F);
-            cyberButton1.ForeColor = Color.FromArgb(245, 245, 245);
-            cyberButton1.Lighting = false;
-            cyberButton1.LinearGradient_Background = false;
-            cyberButton1.LinearGradientPen = false;
-            cyberButton1.Location = new Point(38, 496);
-            cyberButton1.Name = "cyberButton1";
-            cyberButton1.PenWidth = 15;
-            cyberButton1.Rounding = true;
-            cyberButton1.RoundingInt = 70;
-            cyberButton1.Size = new Size(195, 75);
-            cyberButton1.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
-            cyberButton1.TabIndex = 0;
-            cyberButton1.Tag = "Cyber";
-            cyberButton1.TextButton = "CyberButton";
-            cyberButton1.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
-            cyberButton1.Timer_Effect_1 = 5;
-            cyberButton1.Timer_RGB = 300;
+            btnvoltar.Alpha = 20;
+            btnvoltar.BackColor = Color.Transparent;
+            btnvoltar.Background = true;
+            btnvoltar.Background_WidthPen = 4F;
+            btnvoltar.BackgroundPen = true;
+            btnvoltar.ColorBackground = Color.FromArgb(37, 52, 68);
+            btnvoltar.ColorBackground_1 = Color.FromArgb(37, 52, 68);
+            btnvoltar.ColorBackground_2 = Color.FromArgb(41, 63, 86);
+            btnvoltar.ColorBackground_Pen = Color.FromArgb(29, 200, 238);
+            btnvoltar.ColorLighting = Color.FromArgb(29, 200, 238);
+            btnvoltar.ColorPen_1 = Color.FromArgb(37, 52, 68);
+            btnvoltar.ColorPen_2 = Color.FromArgb(41, 63, 86);
+            btnvoltar.CyberButtonStyle = ReaLTaiizor.Enum.Cyber.StateStyle.Custom;
+            btnvoltar.Effect_1 = true;
+            btnvoltar.Effect_1_ColorBackground = Color.FromArgb(29, 200, 238);
+            btnvoltar.Effect_1_Transparency = 25;
+            btnvoltar.Effect_2 = true;
+            btnvoltar.Effect_2_ColorBackground = Color.White;
+            btnvoltar.Effect_2_Transparency = 20;
+            btnvoltar.Font = new Font("Arial", 11F);
+            btnvoltar.ForeColor = Color.FromArgb(245, 245, 245);
+            btnvoltar.Lighting = false;
+            btnvoltar.LinearGradient_Background = false;
+            btnvoltar.LinearGradientPen = false;
+            btnvoltar.Location = new Point(38, 496);
+            btnvoltar.Name = "btnvoltar";
+            btnvoltar.PenWidth = 15;
+            btnvoltar.Rounding = true;
+            btnvoltar.RoundingInt = 70;
+            btnvoltar.Size = new Size(195, 75);
+            btnvoltar.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            btnvoltar.TabIndex = 0;
+            btnvoltar.Tag = "Cyber";
+            btnvoltar.TextButton = "voltar";
+            btnvoltar.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            btnvoltar.Timer_Effect_1 = 5;
+            btnvoltar.Timer_RGB = 300;
+            btnvoltar.Click += btnvoltar_Click;
             // 
             // frmCardapio
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(931, 592);
-            Controls.Add(cyberButton1);
+            Controls.Add(btnvoltar);
             FormBorderStyle = FormBorderStyle.None;
             Name = "frmCardapio";
             StartPosition = FormStartPosition.CenterScreen;
@@ -86,6 +87,6 @@
 
         #endregion
 
-        private ReaLTaiizor.Controls.CyberButton cyberButton1;
+        private ReaLTaiizor.Controls.CyberButton btnvoltar;
     }
 }
--- Comandas/FrmCardapio.cs
+++ Comandas/FrmCardapio.cs
@@ -16,5 +16,10 @@ namespace Comandas
         {
             InitializeComponent();
         }
+
+        private void btnvoltar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
